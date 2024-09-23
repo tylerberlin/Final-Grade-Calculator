06026bdfef9b27ff9966dcdb90c88583625a4e58
tyler0611 <berlity0611@gmail.com>
2024-09-23 10:05:52 -0500

    Initial Commit

---
+++ Final Grade Calculator/Final_Grade_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Final_Grade_CalculatorApp.swift
+//  Final Grade Calculator
+//
+//  Created by Tyler Berlin on 9/23/24.
+//
+
+import SwiftUI
+
+@main
+struct Final_Grade_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Final Grade CalculatorTests/Final_Grade_CalculatorTests.swift
@@ -0,0 +1,17 @@
+//
+//  Final_Grade_CalculatorTests.swift
+//  Final Grade CalculatorTests
+//
+//  Created by Tyler Berlin on 9/23/24.
+//
+
+import Testing
+@testable import Final_Grade_Calculator
+
+struct Final_Grade_CalculatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
